00db3149a7ddf06e23a3906b4118feddf72eeaf1
ema.ferr <78190534+emaferr@users.noreply.github.com>
2021-07-12 23:29:46 +0200

    Add controller add model cicle movies

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -10,91 +10,36 @@
         <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@200;600&display=swap" rel="stylesheet">
 
         <!-- Styles -->
-        <style>
-            html, body {
-                background-color: #fff;
-                color: #636b6f;
-                font-family: 'Nunito', sans-serif;
-                font-weight: 200;
-                height: 100vh;
-                margin: 0;
-            }
-
-            .full-height {
-                height: 100vh;
-            }
-
-            .flex-center {
-                align-items: center;
-                display: flex;
-                justify-content: center;
-            }
-
-            .position-ref {
-                position: relative;
-            }
-
-            .top-right {
-                position: absolute;
-                right: 10px;
-                top: 18px;
-            }
-
-            .content {
-                text-align: center;
-            }
-
-            .title {
-                font-size: 84px;
-            }
-
-            .links > a {
-                color: #636b6f;
-                padding: 0 25px;
-                font-size: 13px;
-                font-weight: 600;
-                letter-spacing: .1rem;
-                text-decoration: none;
-                text-transform: uppercase;
-            }
-
-            .m-b-md {
-                margin-bottom: 30px;
-            }
-        </style>
+        <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+   
     </head>
+
     <body>
-        <div class="flex-center position-ref full-height">
-            @if (Route::has('login'))
-                <div class="top-right links">
-                    @auth
-                        <a href="{{ url('/home') }}">Home</a>
-                    @else
-                        <a href="{{ route('login') }}">Login</a>
-
-                        @if (Route::has('register'))
-                            <a href="{{ route('register') }}">Register</a>
-                        @endif
-                    @endauth
+        <header>
+            <nav class="navbar navbar-dark bg-dark">
+                <div class="container-fluid">
+                  <span class="navbar-brand mb-0 h1">Navbar</span>
                 </div>
-            @endif
+            </nav>
+        </header>
 
-            <div class="content">
-                <div class="title m-b-md">
-                    Laravel
+        <main>
+            <div class="p-5 d-flex flex-wrap justify-content-center">
+                @foreach ($movies as $movie)
+                <div class="card col-3 m-2">
+                    <div class="card-body">
+                        <h3 class="card-text">{{$movie->title}}</h3>
+                        <h3 class="card-text">{{$movie->original_title}}</h3>
+                        <h3 class="card-text">{{$movie->nationality}}</h3>
+                        <h3 class="card-text">{{$movie->date}}</h3>
+                        <h3 class="card-text">{{$movie->vote}}</h3>
+                    </div>
                 </div>
-
-                <div class="links">
-                    <a href="https://laravel.com/docs">Docs</a>
-                    <a href="https://laracasts.com">Laracasts</a>
-                    <a href="https://laravel-news.com">News</a>
-                    <a href="https://blog.laravel.com">Blog</a>
-                    <a href="https://nova.laravel.com">Nova</a>
-                    <a href="https://forge.laravel.com">Forge</a>
-                    <a href="https://vapor.laravel.com">Vapor</a>
-                    <a href="https://github.com/laravel/laravel">GitHub</a>
-                </div>
+                @endforeach
             </div>
-        </div>
+        </main>
+        
+        
+        
     </body>
 </html>
